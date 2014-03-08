f5d9373525e7905a995fbcb206a4738f297ec34c
Jxck <block.rxckin.beats@gmail.com>
2014-03-08 05:31:29 +0000

    fixup

--- goken15-apache-bench/src/jxck/main.go
+++ goken15-apache-bench/src/jxck/main.go
@@ -6,41 +6,68 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"sync"
 	"time"
 )
 
 var (
-	n int64
-	c int
+	n   int
+	c   int
 	url string
 )
 
 func init() {
 	log.SetFlags(log.Lshortfile)
-	flag.Int64Var(&n, "n", 1, "number of requests")
+	flag.IntVar(&n, "n", 1, "number of requests")
 	flag.IntVar(&c, "c", 1, "number of clients")
 	flag.Parse()
 	url = os.Args[len(os.Args)-1]
 }
 
-func main() {
-	start := time.Now()
-	var i int64
-	for i = 0; i < n; i++ {
-		resp, err := http.Get(url)
-		if err != nil {
-			log.Println(resp, err)
+func seq(max int) <-chan int {
+	i := make(chan int)
+	go func() {
+		for {
+			i <- max
+			max = max - 1
+			if max <= 0 {
+				close(i)
+				break
+			}
 		}
+	}()
+	return i
+}
+
+func main() {
+	var wg sync.WaitGroup
+	s := seq(n)
+
+	start := time.Now()
+	for i := 0; i < c; i++ {
+		wg.Add(1)
+		go func() {
+			for _ = range s {
+				resp, err := http.Get(url)
+				if err != nil {
+					log.Println(resp, err)
+				}
+			}
+			wg.Done()
+		}()
 	}
+
+	wg.Wait()
 	total := time.Since(start)
-	avg := time.Duration(total.Nanoseconds() / n)
-	rps := int64(time.Second / avg)
+	avg := (float64(total.Nanoseconds()) / float64(n)) / (1000 * 1000)
+	rps := (float64(n) / float64(total.Nanoseconds())) * (1000 * 1000)
+	log.Println(rps)
 
 	format := `
-total time: %v [ms]
-average time: %v [ms]
-req per sec: %v [#/sec]
+total time: %.4f [s]
+average time: %.4f [ms]
+req per sec: %.4f [#/sec]
 `
 
-	fmt.Printf(format, total, avg, rps)
+	fmt.Printf(format, total.Seconds(), avg, rps)
 }
